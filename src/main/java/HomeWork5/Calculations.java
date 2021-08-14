c07907bb2fecb692309eba18cf312a37bebf1834
Remooke <drew53608@gmail.com>
2021-08-14 12:56:41 +0300

    Задание 5 испр 1

--- src/main/java/HomeWork5/Calculations.java
+++ src/main/java/HomeWork5/Calculations.java
@@ -1,24 +1,33 @@
 package HomeWork5;
 
 public class Calculations {
+    private double result;
+    private double num1;
+    private double num2;
+    private String op;
 
-    public static void calc(double numberOne, String operations, double numberTwo) {
-        double result = 0;
-        switch (operations) {
+    public Calculations(double num1, String op, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.op = op;
+    }
+
+    public void calc() {
+        switch (op) {
             case "+":
-                result = numberOne + numberTwo;
+                result = num1 + num2;
                 System.out.printf("Сумма = %.4f", result);
                 break;
             case "-":
-                result = numberOne - numberTwo;
+                result = num1 - num2;
                 System.out.printf("Разность = %.4f", result);
                 break;
             case "/":
-                result = numberOne / numberTwo;
+                result = num1 / num2;
                 System.out.printf("Частное = %.4f", result);
                 break;
             case "*":
-                result = numberOne * numberTwo;
+                result = num1 * num2;
                 System.out.printf("Произведение = %.4f", result);
                 break;
             default:
--- src/main/java/HomeWork5/Calculator.java
+++ src/main/java/HomeWork5/Calculator.java
@@ -2,13 +2,8 @@ package HomeWork5;
 
 public class Calculator {
 
-    private static double numberTwo;
-    private static double numberOne;
-
     public static void main(String[] args) {
-        numberOne = Read.numReader();// ввод первого числа
-        String operations = Read.operationReader(); //выбор операции
-        numberTwo = Read.numReader(); // ввод второго числа
-        Calculations.calc(numberOne, operations, numberTwo); //вызов вычисления
+       Calculations calculations1 = new Calculations(Read.numReader(),Read.operationReader(), Read.numReader());
+       calculations1.calc();
     }
 }
