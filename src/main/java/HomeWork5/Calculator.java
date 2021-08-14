2474bfd440597cbaefb8ea15919961b5590dd1ea
Remooke <drew53608@gmail.com>
2021-08-14 13:12:37 +0300

    Задание 5 марафет навел немножко

--- src/main/java/HomeWork5/Calculator.java
+++ src/main/java/HomeWork5/Calculator.java
@@ -3,10 +3,10 @@ package HomeWork5;
 public class Calculator {
 
     public static void main(String[] args) {
-        System.out.println("---------ПЕРВОЕ ВЫЧИСЛЕНИЕ--------");
+        System.out.println("---------ПЕРВОЕ ВЫЧИСЛЕНИЕ----------");
         Calculations calculations1 = new Calculations(Read.numReader(), Read.operationReader(), Read.numReader());
         calculations1.calc(); //вычисление и результат
-        System.out.println("---------ВТОРОЕ ВЫЧИСЛЕНИЕ--------");
+        System.out.println("---------ВТОРОЕ ВЫЧИСЛЕНИЕ----------");
         Calculations calculations2 = new Calculations(Read.numReader(), Read.operationReader(), Read.numReader());
         calculations2.calc(); //вычисление и результат
     }
